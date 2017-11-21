ec9e56f11cf9805f090f5eda938b8aa24a06d259
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-11-21 12:14:57 -0500

    Initial commit

---
+++ App_Code/Seguridad.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Web
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+
+' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
+' <System.Web.Script.Services.ScriptService()> _
+<WebService(Namespace:="http://www.loteria.com.ec/")> _
+<WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Public Class Seguridad
+    Inherits System.Web.Services.WebService
+
+    '<WebMethod()> _
+    'Public Function HelloWorld() As String
+    '    Return "Hola a todos"
+    'End Function
+
+    <WebMethod()> _
+    Public Function ValidarCredenciales(ByVal usuario As String, ByVal clave As String, ByVal dominio As String) As Boolean
+        Dim adAuth As LdapAuthentication = New LdapAuthentication("LDAP://")
+        Dim resultado As Boolean = adAuth.ValidarCredenciales(usuario, clave, dominio)
+        Return resultado
+    End Function
+End Class
---
+++ Seguridad.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="vb" CodeBehind="~/App_Code/Seguridad.vb" Class="Seguridad" %>
